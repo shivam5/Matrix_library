af349bf51a289c427f3ea949efe06c91acec9f0e
Shivam Mittal <2015csb1032@iitrpr.ac.in>
2017-03-18 21:55:48 +0530

    First commit

---
+++ include/matrix.h
@@ -0,0 +1,622 @@
+#ifndef _MATRIX_
+#define _MATRIX_
+
+#include <vector>
+#include <iostream>
+
+
+#include <array>
+#include <numeric>
+
+
+template <class data_type>
+class Matrix { 
+	private: 
+		typedef std::vector<data_type> row;
+		typedef std::vector<row> matrix;
+		matrix M;
+		unsigned int row_size;
+		unsigned int col_size;
+
+		std :: vector <data_type> vals;
+
+	public: 
+		// Constructor with no parameters
+		Matrix();
+
+		// Constructor with predefined row size and column size
+		Matrix(const unsigned int r_size, const unsigned int c_size);
+
+		// Copy constructor
+		Matrix (const Matrix &obj);
+
+		void populate_vector() {}
+		template <typename... Args>
+		void populate_vector(data_type value, Args... args);
+
+		template <typename... Args>
+		void set_matrix (data_type value, Args... args);
+
+		template <typename... Args>
+		void set_row (const unsigned int row, data_type value, Args... args);
+
+		template <typename... Args>
+		void set_col (const unsigned int col, data_type value, Args... args);
+
+		void resize (const unsigned int r_size, const  unsigned int c_size);
+		void display_matrix();
+		void display_row(const unsigned int row);
+		void display_col(const unsigned int col);
+
+		void set_value(const unsigned int row, const unsigned int col, const data_type val);
+		data_type get_value(const unsigned int row, const unsigned int col);
+
+		data_type det ();
+		data_type determinant (Matrix <data_type> &mat, int N);
+		void cofactor_matrix (Matrix <data_type> &mat, Matrix <data_type> &cofactor, int p, int q, int N);
+
+		Matrix <float> inv ();
+		void adjoint(Matrix <data_type> &A,Matrix <data_type> &adj);
+		bool inverse(Matrix <data_type> &A, Matrix <float> &inverse);
+
+		template <class d_type> friend 
+		Matrix <d_type> operator + (const Matrix <d_type> &M1, const Matrix <d_type> &M2);
+
+		template <class d_type>	friend 
+		void operator += (Matrix <d_type> &M1, const Matrix <d_type> &M2);
+
+		template <class d_type> friend 
+		Matrix <d_type> operator - (const Matrix <d_type> &M1, const Matrix <d_type> &M2);
+
+		template <class d_type>	friend 
+		void operator -= (Matrix <d_type> &M1, const Matrix <d_type> &M2);
+
+		template <class d_type>	friend 
+		Matrix <d_type> operator * (const Matrix <d_type> &M1, const Matrix <d_type> &M2);
+
+		template <class d_type>	friend 
+		void operator *= (Matrix <d_type> &M1, const Matrix <d_type> &M2);
+
+		template <class d_type> friend 
+		Matrix <d_type> operator + (const Matrix <d_type> &M1, const int x);
+
+		template <class d_type>	friend 
+		void operator += (Matrix <d_type> &M1, const int x);
+
+		template <class d_type> friend 
+		Matrix <d_type> operator - (const Matrix <d_type> &M1, const int x);
+
+		template <class d_type>	friend 
+		void operator -= (Matrix <d_type> &M1, const int x);
+
+		template <class d_type>	friend 
+		Matrix <d_type> operator * (const Matrix <d_type> &M1, const int x);
+
+		template <class d_type>	friend 
+		void operator *= (Matrix <d_type> &M1, const int x);		
+
+		template <class d_type>	friend 
+		Matrix <d_type> operator / (const Matrix <d_type> &M1, const int x);
+
+		template <class d_type>	friend 
+		void operator /= (Matrix <d_type> &M1, const int x);		
+
+
+}; 
+
+
+template <class data_type> 
+Matrix <data_type> :: Matrix(){	
+	row_size = col_size = 0;
+}
+
+template <class data_type> 
+Matrix <data_type> :: Matrix(const unsigned int r_size, const unsigned int c_size){
+	row_size = r_size;
+	col_size = c_size;
+	
+	M.resize(row_size);
+	for (int i=0; i<row_size; i++)
+		M[i].resize(col_size);
+}
+
+template <class data_type> 
+Matrix <data_type> :: Matrix(const Matrix &obj){
+	row_size = obj.M.size();
+	if (row_size > 0)
+		col_size = obj.M[0].size();
+	else
+		col_size = 0;
+	
+	M.resize(row_size);
+
+	for (int i=0; i<row_size; i++){
+		M[i].resize(col_size);
+		for (int j=0; j<col_size; j++)
+			M[i][j] = obj.M[i][j];
+	}
+}
+
+template <class data_type>
+template <typename... Args>
+void Matrix <data_type> :: populate_vector (data_type value, Args... args){
+	vals.push_back(value);
+	if( sizeof...(args)>0 )
+		populate_vector (args... );
+}
+
+template <class data_type>
+template <typename... Args>
+void Matrix <data_type> :: set_matrix (data_type value, Args... args){
+	int n = sizeof...(args)+1;
+	if ( n != (row_size*col_size) ){
+		std :: cerr << "The number of arguments do not match with the dimensions of the matrix."<<std :: endl;
+		return;
+	}
+
+	vals.clear();
+	populate_vector (value, args...);
+
+	for (int i=0; i<row_size; i++){
+		for (int j=0; j<col_size; j++)
+			M[i][j] = vals[i*row_size+j];
+	}
+}
+
+template <class data_type>
+template <typename... Args>
+void Matrix <data_type> :: set_row (const unsigned int row, data_type value, Args... args){
+	int n = sizeof...(args)+1;
+	if ( n != (row_size) ){
+		std :: cerr << "The number of arguments do not match with the row size."<<std :: endl;
+		return;
+	}
+
+	if ( row >= col_size ){
+		std :: cerr << "The column specified is out of bounds"<<std :: endl;
+		return;
+	}
+
+	vals.clear();
+	populate_vector (value, args...);
+
+	for (int i=0; i<row_size; i++)
+		M[row][i] = vals[i];
+}
+
+template <class data_type>
+template <typename... Args>
+void Matrix <data_type> :: set_col (const unsigned int col, data_type value, Args... args){
+	int n = sizeof...(args)+1;
+
+	if ( n != (col_size) ){
+		std :: cerr << "The number of arguments do not match with the column size."<<std :: endl;
+		return;
+	}
+
+	if ( col >= row_size ){
+		std :: cerr << "The column specified is out of bounds"<<std :: endl;
+		return;
+	}
+
+	vals.clear();
+	populate_vector (value, args...);
+
+	for (int i=0; i<row_size; i++)
+		M[i][col] = vals[i];
+}
+
+template <class data_type> 
+void Matrix <data_type> :: resize(const unsigned int r_size, const unsigned int c_size){
+	row_size = r_size;
+	col_size = c_size;
+	
+	M.resize(row_size);
+	for (int i=0; i<row_size; i++)
+		M[i].resize(col_size);
+}
+
+template <class data_type> 
+void Matrix <data_type> :: display_matrix(){
+	for (int i=0; i<row_size; i++){
+		for (int j=0; j<col_size; j++)
+			std :: cout<<M[i][j]<<" ";
+		std :: cout<<std :: endl;
+	}	
+	std :: cout<<std :: endl;
+}
+
+template <class data_type> 
+void Matrix <data_type> :: display_row(const unsigned int row){
+	for (int j=0; j<col_size; j++)
+		std :: cout<<M[row][j]<<" ";
+	std :: cout<<std :: endl;
+}
+
+template <class data_type> 
+void Matrix <data_type> :: display_col(const unsigned int col){
+	for (int i=0; i<row_size; i++)
+		std :: cout<<M[i][col]<<std :: endl;
+	std :: cout<<std :: endl;
+}
+
+template <class data_type> 
+void Matrix <data_type> :: set_value(const unsigned int row, const unsigned int col, const data_type val){
+	M[row][col] = val;
+}
+
+template <class data_type> 
+data_type Matrix <data_type> :: get_value(const unsigned int row, const unsigned int col){
+	return M[row][col];
+}
+
+template <class data_type> 
+data_type Matrix <data_type> :: det (){
+	if ( row_size != col_size ){
+		std :: cerr << "Square matrix required for calculating determinant. Returning 0."<<std::endl;
+		return 0;
+	}
+
+	Matrix <data_type> mat( row_size, col_size);
+
+	for (int i=0; i< row_size; i++){
+		for (int j=0; j< col_size; j++)
+			mat.M[i][j] = M[i][j];
+	}	
+
+	return determinant (mat, row_size);
+
+}
+
+template <class data_type> 
+data_type Matrix <data_type> :: determinant (Matrix <data_type> &mat, int N){
+    
+    if (N == 1)
+        return mat.M[0][0];
+    
+    data_type det = 0;
+
+	Matrix <data_type> cofactor(N, N);
+
+    int sign_multiplier = 1;
+
+    for (int i = 0; i < N; i++){
+        cofactor_matrix(mat, cofactor, 0, i, N);
+        det += (sign_multiplier * mat.M[0][i] * determinant(cofactor, N-1));
+        // std::cout<<"Determinant after "<<i<<"th step."<<det<<std::endl;
+        sign_multiplier = -sign_multiplier;
+    }
+ 
+    return det;
+}
+
+template <class data_type> 
+void Matrix <data_type> :: cofactor_matrix (Matrix <data_type> &mat, Matrix <data_type> &cofactor, int p, int q, int N){
+	int i = 0, j = 0;
+ 
+    for (int row = 0; row < N; row++){
+        for (int col = 0; col < N; col++){
+
+            if (row != p && col != q){
+                cofactor.M[i][j++] = mat.M[row][col];
+ 
+                if (j == N - 1){
+                    j = 0;
+                    i++;
+                }
+            }
+        }
+    }	
+
+}
+
+
+template <class data_type> 
+Matrix <float> Matrix <data_type> :: inv (){
+
+	Matrix <float> inv(row_size, col_size);
+
+	if ( row_size != col_size ){
+		std :: cerr << "Square matrix required for calculating inverse. Returning 0 matrix."<<std::endl;
+		return inv;
+	}
+
+	Matrix <data_type> A( row_size, col_size);
+
+	for (int i=0; i< row_size; i++){
+		for (int j=0; j< col_size; j++)
+			A.M[i][j] = M[i][j];
+	}	
+
+	if (inverse(A, inv))
+		return inv;
+
+	Matrix <float> x(row_size, col_size);
+	return x;
+
+}
+
+template <class data_type> 
+void Matrix <data_type> :: adjoint(Matrix <data_type> &A,Matrix <data_type> &adj){
+	int N = adj.row_size;
+    if (N == 1)
+    {
+        adj.M[0][0] = 1;
+        return;
+    }
+ 
+    int sign = 1;
+    Matrix <data_type> temp(N, N);
+ 
+    for (int i=0; i<N; i++)
+    {
+        for (int j=0; j<N; j++)
+        {
+            cofactor_matrix(A, temp, i, j, N);
+            sign = ((i+j)%2==0)? 1: -1;
+            adj.M[j][i] = (sign)*(determinant(temp, N-1));
+        }
+    }
+}
+
+template <class data_type> 
+bool Matrix <data_type> :: inverse(Matrix <data_type> &A, Matrix <float> &inverse){
+	int N = A.row_size;
+    data_type det = determinant(A, N);
+    if (det == 0)
+    {
+        std::cout << "Singular matrix, can't find its inverse";
+        return false;
+    }
+ 
+    Matrix <data_type> adj(N, N);
+    adjoint(A, adj);
+
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++)
+            inverse.set_value(i, j, adj.get_value(i,j) / (float) det);
+    }
+ 
+    return true;
+}
+
+
+
+
+
+template <class data_type>
+Matrix <data_type> operator+ (const Matrix <data_type> &M1, const Matrix <data_type> &M2) {
+
+	if ( M1.row_size!=M2.row_size || M1.col_size!=M2.col_size ){
+		std :: cerr << "Dimensions of the two matrices are not same for matrix addition. Resultant = First_matrix."<<std::endl;
+		return M1;
+	}
+
+		
+	Matrix <data_type> M3(M1.row_size, M1.col_size);
+
+	for (int i=0; i<M3.row_size; i++){
+		for (int j=0; j<M3.col_size; j++)
+			M3.M[i][j] = M1.M[i][j] + M2.M[i][j];
+	}
+
+	return M3;
+
+}
+
+
+template <class data_type>
+void operator+= (Matrix <data_type> &M1, const Matrix <data_type> &M2) {
+
+	if ( M1.row_size!=M2.row_size || M1.col_size!=M2.col_size ){
+		std :: cerr << "Dimensions of the two matrices are not same for matrix addition. Resultant = First_matrix."<<std::endl;
+		return;
+	}
+
+	for (int i=0; i<M1.row_size; i++){
+		for (int j=0; j<M1.col_size; j++)
+			M1.M[i][j] = M1.M[i][j] + M2.M[i][j];
+	}
+
+}
+
+template <class data_type>
+Matrix <data_type> operator- (const Matrix <data_type> &M1, const Matrix <data_type> &M2) {
+
+	if ( M1.row_size!=M2.row_size || M1.col_size!=M2.col_size ){
+		std :: cerr << "Dimensions of the two matrices are not same for matrix subtraction. Resultant = First_matrix."<<std::endl;
+		return M1;
+	}
+
+		
+	Matrix <data_type> M3(M1.row_size, M1.col_size);
+
+	for (int i=0; i<M3.row_size; i++){
+		for (int j=0; j<M3.col_size; j++)
+			M3.M[i][j] = M1.M[i][j] - M2.M[i][j];
+	}
+
+	return M3;
+
+}
+
+
+template <class data_type>
+void operator-= (Matrix <data_type> &M1, const Matrix <data_type> &M2) {
+
+	if ( M1.row_size!=M2.row_size || M1.col_size!=M2.col_size ){
+		std :: cerr << "Dimensions of the two matrices are not same for matrix subtraction. Resultant = First_matrix."<<std::endl;
+		return;
+	}
+
+	for (int i=0; i<M1.row_size; i++){
+		for (int j=0; j<M1.col_size; j++)
+			M1.M[i][j] = M1.M[i][j] - M2.M[i][j];
+	}
+
+}
+
+
+template <class data_type>
+Matrix <data_type> operator* (const Matrix <data_type> &M1, const Matrix <data_type> &M2) {
+
+	if ( M1.col_size!=M2.row_size )
+		std :: cerr << "Dimensions of the two matrices are not compatible for matrix multiplication."<<std::endl;
+
+		
+	Matrix <data_type> M3(M1.row_size, M2.col_size);
+
+	for (int i=0; i<M1.row_size; i++){
+		for (int j=0; j<M2.col_size; j++){
+			int x=0;
+			for (int k=0; k<M1.col_size; k++){
+				x += (M1.M[i][k] * M2.M[k][j]);
+			}
+			M3.M[i][j] = x;
+		}
+	}
+
+	return M3;
+
+}
+
+template <class data_type>
+void operator*= (Matrix <data_type> &M1, const Matrix <data_type> &M2) {
+
+	if ( M1.col_size!=M2.row_size )
+		std :: cerr << "Dimensions of the two matrices are not compatible for matrix multiplication."<<std::endl;
+		
+	Matrix <data_type> M3(M1.row_size, M2.col_size);
+
+	for (int i=0; i<M1.row_size; i++){
+		for (int j=0; j<M2.col_size; j++){
+			int x=0;
+			for (int k=0; k<M1.col_size; k++){
+				x += M1.M[i][k] * M2.M[k][j];
+			}
+			M3.M[i][j] = x;
+		}
+	}
+
+	M1.resize(M1.row_size, M2.col_size);
+
+	for (int i=0; i<M1.row_size; i++){
+		for (int j=0; j<M1.col_size; j++){
+			M1.M[i][j] = M3.M[i][j];
+		}
+	}
+
+}
+
+
+
+template <class data_type>
+Matrix <data_type> operator+ (const Matrix <data_type> &M1, const int x) {
+		
+	Matrix <data_type> M2(M1.row_size, M1.col_size);
+
+	for (int i=0; i<M2.row_size; i++){
+		for (int j=0; j<M2.col_size; j++)
+			M2.M[i][j] = M1.M[i][j] + x;
+	}
+
+	return M2;
+}
+
+
+template <class data_type>
+void operator+= (Matrix <data_type> &M1, const int x) {
+
+	for (int i=0; i<M1.row_size; i++){
+		for (int j=0; j<M1.col_size; j++)
+			M1.M[i][j] = M1.M[i][j] + x;
+	}
+
+}
+
+template <class data_type>
+Matrix <data_type> operator- (const Matrix <data_type> &M1, const int x) {
+		
+	Matrix <data_type> M2(M1.row_size, M1.col_size);
+
+	for (int i=0; i<M2.row_size; i++){
+		for (int j=0; j<M2.col_size; j++)
+			M2.M[i][j] = M1.M[i][j] - x;
+	}
+
+	return M2;
+}
+
+
+template <class data_type>
+void operator-= (Matrix <data_type> &M1, const int x) {
+
+	for (int i=0; i<M1.row_size; i++){
+		for (int j=0; j<M1.col_size; j++)
+			M1.M[i][j] = M1.M[i][j] - x;
+	}
+
+}
+
+
+template <class data_type>
+Matrix <data_type> operator* (const Matrix <data_type> &M1, const int x) {
+
+	Matrix <data_type> M2(M1.row_size, M1.col_size);
+
+	for (int i=0; i<M2.row_size; i++){
+		for (int j=0; j<M2.col_size; j++)
+			M2.M[i][j] = M1.M[i][j] * x;
+	}
+
+	return M2;
+
+}
+
+template <class data_type>
+void operator*= (Matrix <data_type> &M1, const int x) {
+
+	for (int i=0; i<M1.row_size; i++){
+		for (int j=0; j<M1.col_size; j++)
+			M1.M[i][j] = M1.M[i][j] * x;
+	}
+
+}
+
+template <class data_type>
+Matrix <data_type> operator/ (const Matrix <data_type> &M1, const int x) {
+
+	if ( x==0 ){
+		std :: cerr << "Division by 0 not allowed."<<std :: endl;
+		return M1;
+	}
+
+	Matrix <data_type> M2(M1.row_size, M1.col_size);
+
+	for (int i=0; i<M2.row_size; i++){
+		for (int j=0; j<M2.col_size; j++)
+			M2.M[i][j] = M1.M[i][j] / x;
+	}
+
+	return M2;
+
+}
+
+template <class data_type>
+void operator/= (Matrix <data_type> &M1, const int x) {
+
+	if ( x==0 ){
+		std :: cerr << "Division by 0 not allowed."<<std :: endl;
+		return;
+	}
+
+	for (int i=0; i<M1.row_size; i++){
+		for (int j=0; j<M1.col_size; j++)
+			M1.M[i][j] = M1.M[i][j] / x;
+	}
+
+}
+
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include "./../include/matrix.h"
+#define SIZE 3
+
+int main(){
+
+// Matrix should be sized properly before setting values.
+
+	Matrix <int> M1(SIZE, SIZE);
+
+	M1.set_matrix(0, 1, 2, 3, 4, 5, 6, 7, 8);
+	M1.set_row(0, 10, 11, 12);
+	M1.set_col(2, 13, 14, 15);
+
+	M1.display_matrix();
+
+
+	Matrix <int> M2(M1);
+
+	for (int i=0; i<SIZE; i++){
+		for (int j=0; j<SIZE; j++){
+			M2.set_value(i, j, (i*SIZE+j));
+		}
+	}
+
+	M2.display_matrix();
+
+
+	M1 = M1 / 2;
+
+	M1.display_matrix();
+
+	M2 += 10;
+	M2.display_matrix();
+
+
+	Matrix <int> M3;
+	M3.resize(SIZE, SIZE);
+
+	for (int i=0; i<SIZE; i++){
+		for (int j=0; j<SIZE; j++){
+			M3.set_value(i, j, (i*SIZE+j));
+		}
+	}
+
+	M3.set_row(0, 2, 8, 7);
+	M3.display_matrix();
+
+	std :: cout<<"The determinal of M3 matrix is: "<<M3.det()<<std::endl;
+
+	Matrix <float> M4;
+	M4 = M3.inv();
+	std :: cout<<"The inverse of M3 matrix is:"<<std::endl;
+	M4.display_matrix();
+
+	return 0;
+}
